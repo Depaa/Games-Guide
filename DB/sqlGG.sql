4b713c5789b23edf8a9342d7bf9a68daeb7960c6
Depaa <34038747+Depaa@users.noreply.github.com>
2017-12-03 10:45:57 +0100

    Update sqlGG.sql

--- DB/sqlGG.sql
+++ DB/sqlGG.sql
@@ -14,8 +14,8 @@ create table `Admin`
 
 ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-insert into `Admin` (`Nickname`, `Password`, `Nome`, `Cognome`, `CodFiscale`, `DataNascita`, `Email`)
-values ('admin', 'admin', 'admin', 'admin', 'DMNDMN95R01C743Q', '1995-10-01', 'admin@admin.admin'),
+insert into `Admin` (`Nickname`, `Password`, `Nome`, `Cognome`, `CodFiscale`, `DataNascita`, `Email`) values 
+('admin', 'admin', 'admin', 'admin', 'DMNDMN95R01C743Q', '1995-10-01', 'admin@admin.admin'),
 ('Peachka', 'ciaone', 'Lucia', 'Fenu', 'FNELCU95H70B745B', '1995-06-30', 'lucia.fenu95@gmail.com'),
 ('Aiolos', '123qwerty789', 'Gianmarco', 'Pettenuzzo', 'PTTGMR95M20C743Y', '1995-08-20', 'gian82095@gmail.com'),
 ('Gionny-Atlas', '0987654321', 'Francesco', 'Battistella', 'BTTFRC94R01C743T', '1994-10-01', 'frabat@icloud.com'),
@@ -27,23 +27,23 @@ create table `Notizia`
 	`Titolo` varchar(144) not null unique,
     `AdminNick` varchar(16) not null,
     `Testo` varchar(8192) not null,
-    `XboxOne` int(1),
-    `Xbox360` int(1),
-    `NintendoDS` int(1),
-    `NintendoSwitch` int(1),
-    `PC` int(1),
-    `Android` int(1),
-    `iOS` int(1),
-    `Playstation3` int(1),
-    `Playstation4` int(1),
+    `XboxOne` tinyint(1),
+    `Xbox360` tinyint(1),
+    `NintendoDS` tinyint(1),
+    `NintendoSwitch` tinyint(1),
+    `PC` tinyint(1),
+    `Android` tinyint(1),
+    `iOS` tinyint(1),
+    `Playstation3` tinyint(1),
+    `Playstation4` tinyint(1),
 		
     primary key(`Titolo`)
 )
 
 ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-insert into `Notizie` (`DataPubblicazione`, `AdminNick`, `Titolo`, `Testo`, `XboxOne`, `Xbox360`, `NintendoDS`, `NintendoSwitch`, `PC`, `Android`, `iOS`, `Playstation3`, `Playstation4`)
-values('2017-11-30', 'Depaa','XBOX ONE X: IHS MARKIT HA QUASI RADDOPPIATO LE SUE PREVISIONI DI VENDITA', 
+insert into `Notizie` (`DataPubblicazione`, `AdminNick`, `Titolo`, `Testo`, `XboxOne`, `Xbox360`, `NintendoDS`, `NintendoSwitch`, `PC`, `Android`, `iOS`, `Playstation3`, `Playstation4`) values
+('2017-11-30', 'Depaa','XBOX ONE X: IHS MARKIT HA QUASI RADDOPPIATO LE SUE PREVISIONI DI VENDITA', 
 'Nel corso di un'' intervista concessa a GamesGuide.com, Piers Harding-Rolls, Director of Research and Analysis Games presso IHS Markit, ha confermato che la compagnia di ricerche di mercato ha quasi raddoppiato le proprie previsioni di vendita di Xbox One X nel 2017.
 "Il feedback sui pre-order per Xbox One X standard e la Project Scorpio Edition ci ha spinti a rivedere le nostre previsioni di vendita di Xbox One X nel 2017. La edizione limitata di Xbox One X si è rivelata una mossa vincente, riuscendo a veicolare una robusta quantità di macchine durante la settimana di lancio, nelle Regioni chiave. IHS Markit ha quindi incrementato le proprie previsioni da 500.000 a 900.000 unità".
 Allo stesso modo, la compagnia crede che, al netto di questo possibile successo commerciale, non basterà Xbox One X per colmare il gap presente tra Microsoft e Sony, specialmente prendendo in considerazione lo andamento del mercato in Europa continentale.',
@@ -62,11 +62,10 @@ create table `CommentiNotizie` (
 
 ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-insert into `CommentiNotizie` (`TitoloNot`, `Data`, `Ora`, `UserNick`, `Commento`)
-values('XBOX ONE X: IHS MARKIT HA QUASI RADDOPPIATO LE SUE PREVISIONI DI VENDITA', '2017-11-30', '10:30:00', 'Nikocister',
+insert into `CommentiNotizie` (`TitoloNot`, `Data`, `Ora`, `UserNick`, `Commento`) values
+('XBOX ONE X: IHS MARKIT HA QUASI RADDOPPIATO LE SUE PREVISIONI DI VENDITA', '2017-11-30', '10:30:00', 'Nikocister',
 'La nuova Xbox One X spacca, è molto più performante di tutte le console Sony, sommate tra loro ovviamente ;) e poi, 
-diciamoci la verità, le esclusive della sony non sono nemmeno così carine :sad: '
-);
+diciamoci la verità, le esclusive della sony non sono nemmeno così carine :sad: ');
 
 
 create table `User`
@@ -83,8 +82,8 @@ create table `User`
 
 ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-insert into `User` (`Nickname`, `Nome`, `Cognome`, `Password`, `DataNascita`, `Email`)
-values ('user', 'user', 'user', 'user', '01-10-1995', 'user@user.user'),
+insert into `User` (`Nickname`, `Nome`, `Cognome`, `Password`, `DataNascita`, `Email`) values 
+('user', 'user', 'user', 'user', '01-10-1995', 'user@user.user'),
 ('Nikocister', 'Nicola', 'Cisternino', 'Sassari', '1995-01-12', 'nicocister95@gmail.com'), 
 ('Mmasier', 'Marco', 'Masiero', 'Chubby', '1996-09-09','marcmasi@icloud.com'),
 ('Snordio', 'Stefano', 'Nordio', 'Chioggia4ever', '1995-06-15', 'steno996@gmail.com'),
@@ -109,18 +108,18 @@ create table `Valutazione`
 
 ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-insert into `Valutazione` (`UserNick`, `NomeGioco`, `Voto`)
-values ('Nikocister', 'Outlast II' , 4), 
-('Mmasier', 'FIFA 18' , 3), 
-('Snordio', 'The Legend of Zelda: A Link between world' , 5), 
-('Drakex94', 'Super Mario Odissey' , 4), 
-('DarkWarrior', 'Call of Duty: World War II' , 1),
-('Nikocister', 'Need For Speed: Payback' , 4), 
-('Mmasier', 'Assassin''s Creed: Brotherhood' , 3), 
-('Snordio', 'Clash of Clans' , 5), 
-('Drakex94', 'The Sims 3' , 4), 
-('DarkWarrior', 'Crash Bandicoot: N.Sane Trilogy', 1),
-('DarkWarrior', 'Mortal Combat X', 5);
+insert into `Valutazione` (`UserNick`, `NomeGioco`, `Voto`) values 
+('Nikocister', 'Outlast II' , 4), 
+('Mmasier', 'FIFA18' , 3), 
+('Snordio', 'The Legend Of Zelda - Link Between World' , 5), 
+('Drakex94', 'Mario Odissey' , 4), 
+('DarkWarrior', 'Call of Duty - World War II' , 1),
+('Nikocister', 'Need For Speed - Payback' , 4), 
+('Mmasier', 'Assassin''s Creed - Brotherhood' , 3), 
+('Snordio', 'Clash Of Clans' , 5), 
+('Drakex94', 'The Sims III' , 4), 
+('DarkWarrior', 'Crash - Trilogy', 1),
+('DarkWarrior', 'Mortal Combat V', 5);
 
 create table `Recensione` 
 (
@@ -135,8 +134,8 @@ create table `Recensione`
 
 ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-insert into `Recensione`(`NomeGioco`, `AdminNick`, `Titolo`, `Testo`, `Data`)
-values ('Need For Speed - Payback', 'Aiolos', 'Need For Speed Payback – La vendetta è servita', 'Si parte subito con il prologo in cui si provano alcuni mezzi e viene narrata la storia che apre la modalità carriera. Con grande sorpresa, il sistema di guida adottato è abbastanza realistico e l''aspetto visivo non è per niente male – il tutto è sostenuto dal frostbite, il famoso motore grafico di DICE. La modalità di guida somiglia a "Forza Horizon" di Playground Games, che fa del realismo il proprio cavallo di battaglia, con una grande differenza: il controllo del veicolo. Quest''ultimo forse risulta un po'' troppo eccessivo: è impensabile fare una curva a gomito, a 140, di freno a mano, con una Buick del ''64.
+insert into `Recensione`(`NomeGioco`, `AdminNick`, `Titolo`, `Testo`, `Data`) values 
+('Need For Speed - Payback', 'Aiolos', 'Need For Speed Payback – La vendetta è servita', 'Si parte subito con il prologo in cui si provano alcuni mezzi e viene narrata la storia che apre la modalità carriera. Con grande sorpresa, il sistema di guida adottato è abbastanza realistico e l''aspetto visivo non è per niente male – il tutto è sostenuto dal frostbite, il famoso motore grafico di DICE. La modalità di guida somiglia a "Forza Horizon" di Playground Games, che fa del realismo il proprio cavallo di battaglia, con una grande differenza: il controllo del veicolo. Quest''ultimo forse risulta un po'' troppo eccessivo: è impensabile fare una curva a gomito, a 140, di freno a mano, con una Buick del ''64.
 Tuttavia è un gioco con obiettivi completamente diversi (Qualcuno ha detto Fast & Furious?), quindi sotto questo punto di vista Need for Speed: Payback è giustificato. In modalità Carriera si trovano in mezzo alla strada degli archi azzurro/verdi,che danno il via ad alcune come Autovelox e Salto, dove si deve rispettivamente passare dal varco ad una velocità maggiore di quella indicata oppure  superare i metri richiesti con un salto. La Gara di velocità, dove quello che conta è la media tra il primo e l''ultimo varco – non la velocità istantanea raggiunta nei check point – costa qualche tentativo prima di capire come funzioni. 
 Oltre le nuove modalità, forse un po'' macchinose, ci son le gare "classiche", disponibili nelle categorie corsa, fuoristrada, derapata, accelerazione e fuga (con relative auto di categoria) ed alla fine di ogni gara vengono consegnati dei crediti e delle Speed Card. Le gare hanno una struttura classica, ovvero si deve attraversare i vari checkpoint e concludere primi. Sembra facile detta così. L''intelligenza artificiale dei piloti contro cui si gareggia è talvolta un po'' troppo competitiva: non mancano situazioni in cui si ricevono e si danno sportellate, cambia solo il modo in cui le si danno. Nella modalità corsa è sufficiente spingere un veicolo nemico fuori dalla carreggiata per vedere la sua velocità scendere, lo stesso giochetto però non si riesce ad applicare nelle gare fuoristrada, dove appunto le auto sono pensate per tutti i tipi di terreni. 
 Parlando di differenze, questi sono i principali tipi di gare: Corsa: Gara classica su asfalto, il primo a tagliare il traguardo vince. Fuoristrada: Gara classica con tratti offroad, il primo arrivato vince. Derapata: Gara di derapate, il cambio di terreno non va ad intaccare i punti che vengono dati, il punteggio più alto vince. Accelerazione: l''erede di "sparo" dei capitoli Underground della saga. Rettilineo, cambio manuale, il più veloce vince. Fuga: scappare dai poliziotti e farne fuori il più possibile, non penso necessiti di ulteriori spiegazioni. Le Speed Card rappresentano le modifiche che potete fare al vostro veicolo, di diversi livelli e diverse marche, combinandone tre dello stesso marchio otterrete un bonus diverso in base al brand. 
@@ -158,70 +157,11 @@ create table `CommentiRecensione` (
 
 ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-insert into `CommentiRecensiome` (`TitoloRec`, `Data`, `Ora`, `UserNick`, `Commento`)
-values('Need For Speed Payback – La vendetta è servita', '2017-11-30', '10:30:00', 'Nikocister',
-'molto bella la grafica, ho perso un semestre a causa di questo gioco');
+insert into `CommentiRecensiome` (`TitoloRec`, `Data`, `Ora`, `UserNick`, `Commento`) values
+('Need For Speed Payback – La vendetta è servita', '2017-11-30', '10:30:00', 'Nikocister',
+'molto bella la grafica, ho perso un semestre a causa di questo gioco'
+);
 
-create table `Genere` (
-	`CodGenere` int not null,
-	`NomeGen` varchar (20) not null,
-    
-    primary key (`CodGenere`)
-)
-
-ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
-insert into `Genere` (`CodGenere`, `NomeGen`)
-values (0001, 'Action'),
-		(0002, 'FPS'),
-		(0003, 'RPG'),
-		(0004, 'Adventure'),
-		(0005, 'Fighting'),
-		(0006, 'Platform'),
-		(0007, 'Horror'),
-		(0008, 'Simulatore'),
-		(0009, 'Sport'),
-		(0010, 'Race'),
-		(0011, 'Strategia');
-
-
-create table `Giochi` (
-	`Nome` varchar (100) not null,
-	`DataPub` date not null,
-	`Genere1` varchar (20) not null,
-	`Genere2` varchar (20),
-	`Genere3` varchar (20),
-	`PEGI` int (5),
-	`Valutazione` double not null,
-	`Playstation3` int (1),
-	`Playstation4` int (1),
-	`Xbox360` int (1),
-	`XboxOne` int (1),
-	`NintendoDS` int (1),
-	`NintendoSwitch` int (1),
-	`PC` int (1),
-	`iOS` int (1),
-	`Android` int (1),
-    
-	foreign key `Giochi` (`Genere1`, `Genere2`, `Genere3`),
-	references `Genere` (`CodGenere`)
-)
-
-ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
-insert into `Giochi` (`Nome`, `DataPub`, `Genere1`, `Genere2`, `Genere3`, `PEGI`,`Valutazione`, 
-				`Playstation3`, `Playstation4`, `Xbox360`,`XboxOne`, `NintendoDS`, `NintendoSwitch`, `PC`, `iOS`, `Android`)
-values ('Need For Speed: Payback', '2017-11-10', 'Race', 'Action', null, 12, null, 0,1,0,1,0,0,1,0,0),
-       ('Assassin''s Creed: Brotherhood', '2010-11-16', 'RPG', 'Adventure', null, 18, null,1,1,1,1,0,0,1,0,0),
-       ('Call of Duty: World War II', '2017-11-03', 'FPS', 'Action', null, 18, null, 0,1,0,1,0,0,1,0,0),
-       ('The Legend of Zelda: A Link between worlds', '2013-11-22', 'Adventure','RPG', null, 7, null, 0,0,0,0,1,0,0,0,0),
-       ('Outlast II', '2017-04-25', 'Horror', 'Action', null, 18, null, 0,1,0,1,0,0,1,0,0),
-       ('Clash of Clans', '2012-08-02', 'Strategia', null, null,9, null, 0,0,0,0,0,0,0,1,1),
-       ('The Sims 3', '2009-06-02', 'Simulatore', null, null,13, null, 1,0,1,1,1,0,1,1,1),
-       ('Mortal Kombat X', '2015-04-07', 'Horror', 'Fighting', null,18, null, 0,1,0,1,0,0,1,1,1),
-       ('FIFA 18', '2017-09-29', 'Sport', null, null, 3, null, 1,1,1,1,0,1,1,0,0),
-       ('Crash Bandicoot: N.Sane Trilogy', '2017-06-30', 'Platform', 'Action', 'Adventure', 7, null, 0,1,0,0,0,0,0,0,0),
-       ('Super Mario Oddyssey', '2017-10-27', 'Adventure','Action', null, 10, null, 0,0,0,0,0,1,0,0,0);
 
 /*
 
