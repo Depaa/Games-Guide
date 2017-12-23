d037a49ccc42dd18409c053d99c770d669af2a76
Gian82095 <34053198+Gian82095@users.noreply.github.com>
2017-12-23 16:43:01 +0100

    Update aggiungi.php

--- PHP/aggiungi.php
+++ PHP/aggiungi.php
@@ -2,17 +2,92 @@
 	require "config.php";
 	function Aggiungi()
 	{
-		session_start();
+		
+		
 		$conn = connessione();
-		$query= "INSERT INTO `giochi` (`Nome`, `DataPub`, `Genere1`, `Genere2`, `Genere3`, `PEGI`,`Valutazione`, `Disponibilita`, `Playstation3`, `Playstation4`, `Xbox360`,`XboxOne`, `NintendoDS`, `NintendoSwitch`, `Windows`, `Mac`, `Descrizione`)
-		values  ('$_POST[gname]', '$_POST[gdata]', '$_POST[ggenere1]', null , null, '$_POST[gpegi]', null, '$_POST[gdisp]', '$_POST[p3]', '$_POST[p4]', '$_POST[x3]', '$_POST[xo]', '$_POST[ds]', '$_POST[sw]', '$_POST[win]', '$_POST[mac]', '$_POST[gdesc]');";
-		$query2="insert into `Immagini` (`NomeGioco`, `MenuImg`, `GiocoImg`)	values ('$_POST[gname]', 'NFSPayback.jpg', 'NFSPayback0.jpg');";
+		if (isset($_POST['xo']))
+		{
+			$xo = "1";
+		} 
+		else 
+		{
+			$xo = "0";
+		}
+		
+		if (isset($_POST['x3']))
+		{
+			$x3 = "1";
+		} 
+		else 
+		{
+			$x3 = "0";
+		}
+		
+		if (isset($_POST['p3']))
+		{
+			$p3 = "1";
+		} 
+		else 
+		{
+			$p3 = "0";
+		}
+		
+		if (isset($_POST['p4']))
+		{
+			$p4 = "1";
+		} 
+		else 
+		{
+			$p4 = "0";
+		}
+		
+		if (isset($_POST['sw']))
+		{
+			$sw = "1";
+		} 
+		else 
+		{
+			$sw = "0";
+		}
+		
+		if (isset($_POST['ds']))
+		{
+			$ds = "1";
+		} 
+		else 
+		{
+			$ds = "0";
+		}
+		
+		if (isset($_POST['win']))
+		{
+			$win = "1";
+		} 
+		else 
+		{
+			$win = "0";
+		}
+		
+		if (isset($_POST['mac']))
+		{
+			$mac = "1";
+		} 
+		else 
+		{
+			$mac = "0";
+		}
+		session_start();
+		$query= "INSERT INTO `giochi` (`Nome`, `DataPub`, `Genere1`, `Genere2`, `Genere3`, `PEGI`, `Disponibilita`, `Playstation3`, `Playstation4`, `Xbox360`, `XboxOne`, `NintendoDS`, `NintendoSwitch`, `Windows`, `Mac`, `Descrizione`)
+		values  ('$_POST[gname]', '$_POST[gdata]', '$_POST[ggenere1]', '$_POST[ggenere2]' , '$_POST[ggenere3]', '$_POST[gpegi]', '$_POST[gdisp]', '". $p3. "', '" .$p4. "', '" .$x3. "', '" .$xo. "', '".$ds. "', '" .$sw. "', '" .$win. "', '" .$mac."', '$_POST[gdesc]');";
 		$conn->query($query) or die("Errore nella query MySQL: ".$conn->error);
-		$conn->query($query2) or die("Errore nella query MySQL: ".$conn->error);
 		$conn->close();
 	}
+	
+	
 	if(isset($_POST['confirm'])) 
 	{
+		
+		
 		Aggiungi();
 	}
 ?>
