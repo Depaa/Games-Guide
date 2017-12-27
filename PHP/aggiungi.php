b5c649fb7967f8685fddb4fe206a9ecb8eab974d
Depaa <34038747+Depaa@users.noreply.github.com>
2017-12-27 10:51:28 +0100

    aggiungi completo con bug su immagine

--- PHP/aggiungi.php
+++ PHP/aggiungi.php
@@ -1,93 +1,48 @@
 <?php
 	require "config.php";
+	
+	
 	function Aggiungi()
 	{
-		
-		
-		$conn = connessione();
-		if (isset($_POST['xo']))
-		{
-			$xo = "1";
-		} 
-		else 
-		{
-			$xo = "0";
-		}
-		
-		if (isset($_POST['x3']))
-		{
-			$x3 = "1";
-		} 
-		else 
-		{
-			$x3 = "0";
-		}
-		
-		if (isset($_POST['p3']))
-		{
-			$p3 = "1";
-		} 
-		else 
-		{
-			$p3 = "0";
-		}
-		
-		if (isset($_POST['p4']))
-		{
-			$p4 = "1";
-		} 
-		else 
-		{
-			$p4 = "0";
-		}
-		
-		if (isset($_POST['sw']))
-		{
-			$sw = "1";
-		} 
-		else 
-		{
-			$sw = "0";
-		}
-		
-		if (isset($_POST['ds']))
-		{
-			$ds = "1";
-		} 
-		else 
-		{
-			$ds = "0";
-		}
-		
-		if (isset($_POST['win']))
-		{
-			$win = "1";
-		} 
-		else 
-		{
-			$win = "0";
-		}
-		
-		if (isset($_POST['mac']))
-		{
-			$mac = "1";
-		} 
-		else 
-		{
-			$mac = "0";
-		}
 		session_start();
-		$query= "INSERT INTO `giochi` (`Nome`, `DataPub`, `Genere1`, `Genere2`, `Genere3`, `PEGI`, `Disponibilita`, `Playstation3`, `Playstation4`, `Xbox360`, `XboxOne`, `NintendoDS`, `NintendoSwitch`, `Windows`, `Mac`, `Descrizione`)
-		values  ('$_POST[gname]', '$_POST[gdata]', '$_POST[ggenere1]', '$_POST[ggenere2]' , '$_POST[ggenere3]', '$_POST[gpegi]', '$_POST[gdisp]', '". $p3. "', '" .$p4. "', '" .$x3. "', '" .$xo. "', '".$ds. "', '" .$sw. "', '" .$win. "', '" .$mac."', '$_POST[gdesc]');";
+		$conn = connessione();
+		
+		$query= "INSERT INTO `giochi` (`Nome`, `DataPub`, `Genere1`, `Genere2`, `Genere3`, `PEGI`, `Disponibilita`, `Playstation3`, `Playstation4`, `Xbox360`,`XboxOne`, `NintendoDS`, `NintendoSwitch`, `Windows`, `Mac`, `Descrizione`)
+		values  ('$_POST[gname]', '$_POST[gdata]', '$_POST[ggenere1]', '$_POST[ggenere2]' , '$_POST[ggenere3]', '$_POST[gpegi]', '$_POST[gdisp]', '". (isset($_POST['p3'])). "', '". (isset($_POST['p4'])). "', '". (isset($_POST['x3'])). "', '". (isset($_POST['xo'])). "', '". (isset($_POST['ds'])). "', '". (isset($_POST['sw'])). "', '". (isset($_POST['win'])). "', '". (isset($_POST['mac'])). "', '$_POST[gdesc]');";
+		
+		$nuovo_nome=basename($_FILES["gimgvid"]["name"]);//nuovo nome dell'immagine
+		$nuovo_nome_vid=basename($_FILES["gimgvid"]["name"]."0");
+		
+		
+		$query2="insert into `Immagini` (`NomeGioco`, `MenuImg`, `GiocoImg`) 
+		values ('$_POST[gname]', '$nuovo_nome', '$nuovo_nome_vid');";
 		$conn->query($query) or die("Errore nella query MySQL: ".$conn->error);
+		$conn->query($query2) or die("Errore nella query MySQL: ".$conn->error);
+		
+		$file_temp=$_FILES['gimgmen']['tmp_name'];
+		$file_temp_vid=$_FILES['gimgvid']['tmp_name']; //file temporaneo che contiene l'immagine caricata
+		
+		$percorso="IMG/"; //cartella sul server dove verrà spostata la foto
+		
+		$nuovo_nome=$percorso . basename($_FILES["gimgvid"]["name"]);//nuovo nome dell'immagine
+		$nuovo_nome_vid=$percorso . basename($_FILES["gimgvid"]["name"]."0");
+		
+		$inviato=file_exists($file_temp);
+		$inviato_vid=file_exists($file_temp_vid); //verifica se il file è stato caricato sul server
+		
+		if ($inviato) {
+			move_uploaded_file($_FILES["gimgmen"]["tmp_name"],$nuovo_nome);
+			header("Admin.php"); // sposto l'immagine nella cartella e vado alla pagina di visualizzazione
+		}
+		
+		if ($inviato_vid) {
+			move_uploaded_file($_FILES["gimgvid"]["tmp_name"],$nuovo_nome_vid);
+			header("Admin.php"); // sposto l'immagine nella cartella e vado alla pagina di visualizzazione
+		}
 		$conn->close();
 	}
-	
-	
-	if(isset($_POST['confirm'])) 
+	if(isset($_POST['submit'])) 
 	{
-		
-		
 		Aggiungi();
 	}
 ?>
