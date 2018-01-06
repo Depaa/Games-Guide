37db6380dac640a0799bc55a3747bf4d9198dafe
Depaa <34038747+Depaa@users.noreply.github.com>
2018-01-06 22:23:42 +0100

    filtro+paging non ha sol, è un caso perso addio

--- SERVER/filtro.php
+++ SERVER/filtro.php
@@ -51,12 +51,11 @@
 		if(isset($_POST['hor']))
 			$wh .= " AND (Videogiochi.Genere1='Horror' OR Videogiochi.Genere2='Horror' OR Videogiochi.Genere3='Horror')";
 		
-		$query= "SELECT IDr, Videogiochi.ID, Nome, GiocoImg, Genere1, Genere2, Genere3, Disponibilita, Videogiochi.Data, PEGI, Descrizione FROM Videogiochi JOIN Immagini ON Videogiochi.Nome=Immagini.NomeGioco JOIN Recensione ON Videogiochi.Nome=Recensione.NomeGioco $wh ORDER BY Videogiochi.Data DESC";
+		$query= "SELECT IDr, Videogiochi.ID, Nome, GiocoImg, Genere1, Genere2, Genere3, Disponibilita, Videogiochi.Data, PEGI, Descrizione FROM Videogiochi JOIN Immagini ON Videogiochi.Nome=Immagini.NomeGioco JOIN Recensione ON Videogiochi.Nome=Recensione.NomeGioco $wh ORDER BY Videogiochi.Data DESC LIMIT 100 OFFSET 1";
 		$output = $conn->query($query) or die("Errore nella query MySQL: ".$conn->error);
 	}
 
 	$wh="WHERE 1=1 ";
-	$ord="";
 	if(isset($_POST['submitRP'])) {
 		if(isset($_POST['xo']))
 			$wh .= " AND XboxOne=1";
@@ -75,17 +74,17 @@
 		if(isset($_POST['mac']))
 			$wh .= " AND Mac=1";
 		
-		$ord = " ORDER BY Data DESC LIMIT 100 OFFSET 1";
+		//$inizio2=$inizio+1;
 		$query1 = "SELECT Recensione.NomeGioco, IDr, AdminNick, Testo, Titolo, Data, MenuImg FROM Recensione JOIN Immagini ON Recensione.NomeGioco=Immagini.NomeGioco $wh ORDER BY Data DESC LIMIT 1";
-		$query = "SELECT Recensione.NomeGioco, IDr, AdminNick, Testo, Titolo, Data, MenuImg FROM Recensione JOIN Immagini ON Recensione.NomeGioco=Immagini.NomeGioco $wh $ord";
+		/*$query = "SELECT Recensione.NomeGioco, IDr, AdminNick, Testo, Titolo, Data, MenuImg FROM Recensione JOIN Immagini ON Recensione.NomeGioco=Immagini.NomeGioco $wh ORDER BY Data DESC LIMIT $fine OFFSET $inizio2";*/
+		
+		$query = "SELECT Recensione.NomeGioco, IDr, AdminNick, Testo, Titolo, Data, MenuImg FROM Recensione JOIN Immagini ON Recensione.NomeGioco=Immagini.NomeGioco $wh ORDER BY Data DESC LIMIT 100 OFFSET 1";
 		
 		$output = $conn->query($query) or die("Errore nella query MySQL: ".$conn->error);
-		
 		$output1 = $conn->query($query1) or die("Errore nella query MySQL: ".$conn->error);
 	}
 	
 	$wh="WHERE 1=1 ";
-	$ord="";
 	if(isset($_POST['submitRD'])) {
 		if(isset($_POST['2018']))
 			$wh .= " AND Data>'2017-12-31' AND Data <'2018-12-31'";
@@ -100,18 +99,14 @@
 		else if(isset($_POST['2013']))
 			$wh .= " AND Data>'2012-12-31' AND Data <'2013-12-31'";
 		
-		$ord = " ORDER BY Data DESC LIMIT 100 OFFSET 1";
-		
 		$query1 = "SELECT Recensione.NomeGioco, IDr, AdminNick, Testo, Titolo, Data, MenuImg FROM Recensione JOIN Immagini ON Recensione.NomeGioco=Immagini.NomeGioco $wh ORDER BY Data DESC LIMIT 1";
-		$query = "SELECT Recensione.NomeGioco, IDr, AdminNick, Testo, Titolo, Data, MenuImg FROM Recensione JOIN Immagini ON Recensione.NomeGioco=Immagini.NomeGioco $wh $ord";
+		$query = "SELECT Recensione.NomeGioco, IDr, AdminNick, Testo, Titolo, Data, MenuImg FROM Recensione JOIN Immagini ON Recensione.NomeGioco=Immagini.NomeGioco $wh  ORDER BY Data DESC LIMIT 100 OFFSET 1";
 		
 		$output = $conn->query($query) or die("Errore nella query MySQL: ".$conn->error);
-		
 		$output1 = $conn->query($query1) or die("Errore nella query MySQL: ".$conn->error);
 	}
 	
 	$wh="WHERE 1=1 ";
-	$ord="";
 	if(isset($_POST['submitNP'])) {
 		if(isset($_POST['xo']))
 			$wh .= " AND XboxOne=1";
@@ -130,18 +125,14 @@
 		if(isset($_POST['mac']))
 			$wh .= " AND Mac=1";
 		
-		$ord = " ORDER BY Data DESC LIMIT 100 OFFSET 1";
-		
 		$query1 = "SELECT ID, AdminNick, Testo, Titolo, Data, MenuImg FROM Notizie $wh ORDER BY Data DESC LIMIT 1";
-		$query = "SELECT ID, AdminNick, Testo, Titolo, Data, MenuImg FROM Notizie  $wh $ord";
+		$query = "SELECT ID, AdminNick, Testo, Titolo, Data, MenuImg FROM Notizie $wh ORDER BY Data DESC LIMIT 100 OFFSET 1";
 		
 		$output = $conn->query($query) or die("Errore nella query MySQL: ".$conn->error);
-		
 		$output1 = $conn->query($query1) or die("Errore nella query MySQL: ".$conn->error);
 	}
 	
 	$wh="WHERE 1=1 ";
-	$ord="";
 	if(isset($_POST['submitND'])) {
 		if(isset($_POST['2018']))
 			$wh .= " AND Data>'2017-12-31' AND Data <'2018-12-31'";
@@ -156,13 +147,10 @@
 		else if(isset($_POST['2013']))
 			$wh .= " AND Data>'2012-12-31' AND Data <'2013-12-31'";
 		
-		$ord = " ORDER BY Data DESC LIMIT 100 OFFSET 1";
-		
 		$query1 = "SELECT ID, AdminNick, Testo, Titolo, Data, MenuImg FROM Notizie $wh ORDER BY Data DESC LIMIT 1";
-		$query = "SELECT ID, AdminNick, Testo, Titolo, Data, MenuImg FROM Notizie  $wh $ord";
+		$query = "SELECT ID, AdminNick, Testo, Titolo, Data, MenuImg FROM Notizie $wh ORDER BY Data DESC LIMIT 100 OFFSET 1";
 		
 		$output = $conn->query($query) or die("Errore nella query MySQL: ".$conn->error);
-		
 		$output1 = $conn->query($query1) or die("Errore nella query MySQL: ".$conn->error);
 	}
 ?>
