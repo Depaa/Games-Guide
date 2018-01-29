60d591358fed1547d5f90f377e2e8f8ed4b3fa27
Depaa <34038747+Depaa@users.noreply.github.com>
2018-01-29 10:22:40 +0100

    Update formControls.js

--- JS/formControls.js
+++ JS/formControls.js
@@ -6,8 +6,8 @@ function checkFormL() {
 		alert("Inserire uno Username");
 		return false;
 	}
-	else if (password.length<5) {
-		alert("Password deve essere di almeno 5 caratteri");
+	else if (password.length<4) {
+		alert("Password deve essere di almeno 4 caratteri");
 		return false;
 	}
 }
