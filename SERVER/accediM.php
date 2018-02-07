5dce50ebe0c9ff318ea327b2357bea62a5bc447f
Depaa <34038747+Depaa@users.noreply.github.com>
2018-02-07 10:38:24 +0100

    Update accediM.php

--- SERVER/accediM.php
+++ SERVER/accediM.php
@@ -23,8 +23,6 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 		<script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
-		
-		<script type="text/javascript" src="CSS/script.js"></script>
 	</head>
 
 	<body>
