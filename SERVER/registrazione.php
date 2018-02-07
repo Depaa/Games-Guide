d1c8b5dd58e2f39c8078cca814149fb1b27a436d
Depaa <34038747+Depaa@users.noreply.github.com>
2018-02-07 19:36:53 +0100

    Update registrazione.php

--- SERVER/registrazione.php
+++ SERVER/registrazione.php
@@ -45,7 +45,7 @@
 			$error_message = "Email gia usata";
 		}
 		else {
-			$query= "INSERT INTO `Account` (`Nickname`, `Password`, `Nome`, `Cognome`, `DataNascita`, `Email`) VALUES 
+			$query= "INSERT INTO `account` (`Nickname`, `Password`, `Nome`, `Cognome`, `DataNascita`, `Email`) VALUES 
 			('$_POST[unick]', '$_POST[upsw]', '$_POST[fname]', '$_POST[lname]' , '".date("Y-m-d", strtotime($_POST['dnascita']))."', '$_POST[email]');";
 			
 			$output = $conn->query($query) or die("Erroreeeeeeeee nella query MySQL: ".$conn->error);
@@ -54,7 +54,7 @@
 				$success_message = "Complimenti! Ti sei registrato con successo!";	
 				unset($_POST);
 			} else {
-				$success_message = "Problemi con la registrazione. Riprova.";	
+				$success_message = "Problemi con la registrazione :( Riprova.";	
 			}
 		}
 	}
@@ -63,23 +63,12 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 	<html xmlns="http://www.w3.org/1999/xhtml" lang="it" xml:lang="it">
 	<head>
-		<title>Games'Guide: Registrazione</title>
-		<meta name="description" content="Entra nella community di Games'Guide, troverai tutte le recensioni e le notizie sui giochi più recenti">
-		<meta name="author" content="Lucia Fenu, Francesco Battistella, Gianmarco Pettenuzzo, Matteo Depascale">
-		<meta name="keywords" content="Videogiochi, Notizie, Recensioni, Registrazione">
-		<meta name="robots" content="index, follow">
-		
+		<title>GamesGuide</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-		<link type="text/css" rel="stylesheet" href="CSS/Style.css" media="handheld, screen"/>
-		<link rel="stylesheet" type="text/css" href="CSS/StyleM.css" media="handheld, screen and (max-width:651px), only screen and (max-device-width:651px)"/>
-		<link type="text/css" rel="stylesheet" href="CSS/StyleP.css" media="print"/>
 		
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+		<link type="text/css" rel="stylesheet" href="CSS\Style.css" media="handheld, screen"/>
 		
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-		<meta name="viewport" content="width=device-width, initial-scale=1"/>
-		<script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
-		<script type="text/javascript" src="JS/goTop.js"></script>		
 	</head>
 	
 	<body>
@@ -87,66 +76,55 @@
 			include 'Menu.php';
 		?>
 		
-		<script type="text/javascript" src="JS/formControls.js"></script>
-		
 		<div class="pagecontent">
 			<div class="registrazione-contenuto">
-				<form name="registerForm" method="post" action="" onsubmit="return checkFormR()">
-					<fieldset>
-						<?php if($success_message) { ?>
-						<div class="riga">
-							<h4 id="contenutoPrincipale"><?php if(isset($success_message)) echo $success_message; ?></h4>
-							<h5>Stai per essere reinderizzato alla Home fra 10 secondi, clicca <a href="index.php">Qui</a> se non vuoi aspettare</h5>
-							<script type="text/javascript" src="JS/indexRedirect.js"></script>
-						</div>
-						<?php }
-						else {?>
-						<?php if($error_message) { ?>	
-						<div class="riga">
-							<h4 id="contenutoPrincipale"><?php if(isset($error_message)) echo $error_message; ?></h4>
-						</div>
-						<?php } ?>
-						<div class="riga">
-							<h4 id="contenutoPrincipale">Nome</h4>
-							<input type="text" name="fname" value="<?php if(isset($_POST['fname'])) echo $_POST['fname']; ?>"/> 
-						</div>
-						<div class="riga">	
-							<h4>Cognome</h4>
-							<input type="text" name="lname" value="<?php if(isset($_POST['lname'])) echo $_POST['lname']; ?>"/>
-						</div>
-						<div class="riga">
-							<h4>Data di Nascita (gg-mm-aaaa)</h4>
-							<input type="text" name="dnascita" value="<?php if(isset($_POST['dnascita'])) echo $_POST['dnascita']; ?>"/>
-						</div>
-						<div class="riga">
-						   <h4><span lang="en">Email</span></h4>
-							<input type="text" name="email" value="<?php if(isset($_POST['email'])) echo $_POST['email']; ?>"/>
-						</div>
-						<div class="riga">
-							<h4><span lang="en">Nickname</span></h4>
-							<input type="text" name="unick" value="<?php if(isset($_POST['unick'])) echo $_POST['unick']; ?>"/>
-						</div>
-						<div class="riga">	
-							<h4><span lang="en">Password</span></h4>
-							<input type="password" name="upsw" value=""/>
-						</div>
-						<div class="riga">	
-							<h4>Conferma <span lang="en">Password</span></h4>
-							<input type="password" name="confirmupsw" value=""/>
-						</div>
-						<button class="registrazionebtn" type="submit" name="submitR">REGISTRATI</button>
-					</fieldset>
-				</form>
+				<form method="post" action="">
+					<?php if($success_message) { ?>
+					<div class="riga">
+						<h4><?php if(isset($success_message)) echo $success_message; ?></h4>
+					</div>
 					<?php } ?>
+					<?php if($error_message) { ?>	
+					<div class="riga">
+						<h4><?php if(isset($error_message)) echo $error_message; ?></h4>
+					</div>
+					<?php } ?>
+					<div class="riga">
+						<h4>Nome</h4>
+						<input type="text" name="fname" value="<?php if(isset($_POST['fname'])) echo $_POST['fname']; ?>"/> 
+					</div>
+					<div class="riga">	
+						<h4>Cognome</h4>
+						<input type="text" name="lname" value="<?php if(isset($_POST['lname'])) echo $_POST['lname']; ?>"/>
+					</div>
+					<div class="riga">
+						<h4>Data di Nascita (gg-mm-aaaa)</h4>
+						<input type="text" name="dnascita" value="<?php if(isset($_POST['dnascita'])) echo $_POST['dnascita']; ?>"/>
+					</div>
+					<div class="riga">
+					   <h4>Email</h4>
+						<input type="text" name="email" value="<?php if(isset($_POST['email'])) echo $_POST['email']; ?>"/>
+					</div>
+					<div class="riga">
+						<h4>Nickname</h4>
+						<input type="text" name="unick" value="<?php if(isset($_POST['unick'])) echo $_POST['unick']; ?>"/>
+					</div>
+					<div class="riga">	
+						<h4>Password</h4>
+						<input type="password" name="upsw" value=""/>
+					</div>
+					<div class="riga">	
+						<h4>Conferma Password</h4>
+						<input type="password" name="confirmupsw" value=""/>
+					</div>
+					<p><button class="registrazionebtn" type="submit" name="submitR">REGISTRATI</button></p>
+				</form>
 			</div>
-		<div id="backtotop" class="backtotop">
-			<button onclick="gotopFunction()"><em class="fa fa-arrow-circle-up"></em></button>
-		</div>
 		</div>
 		
-<?php 
-	include 'Footer.php';
-?>
+		<div class="footer">
+			<h4>Progetto del corso di Tecnologie Web anno 2017-2018</h4>
+		</div>
 		
 	</body>
 </html>
