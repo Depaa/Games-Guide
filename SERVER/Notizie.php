c03b7a6e16258bfc3cd7cac1a661d9cf1ed91374
Gian82095 <34053198+Gian82095@users.noreply.github.com>
2018-02-08 21:45:20 +0100

    validato

--- SERVER/Notizie.php
+++ SERVER/Notizie.php
@@ -64,7 +64,7 @@
 		
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
 		
-		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 		<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.js"></script>
 		<script type="text/javascript" src="JS/goTop.js"></script>
