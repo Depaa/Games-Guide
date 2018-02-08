cd0d4be15f21cb88bd03fac8fafa83fc9bf94903
Depaa <34038747+Depaa@users.noreply.github.com>
2018-02-08 16:00:27 +0100

    Update indexRedirect.js

--- JS/indexRedirect.js
+++ JS/indexRedirect.js
@@ -1,7 +1,4 @@
-	function redirect() {
-
-		location.href = "index.php";
-
-	}
-
-	window.setTimeout("redirect()", 10000);
+function redirect() {
+	location.href = "index.php";
+}
+window.setTimeout("redirect()", 10000);
