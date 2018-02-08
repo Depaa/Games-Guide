32e54ff23bec5b6a3107a4fb5cbf8c6f15b95aa2
Depaa <34038747+Depaa@users.noreply.github.com>
2018-02-08 21:42:03 +0100

    validato

--- SERVER/registrazione.php
+++ SERVER/registrazione.php
@@ -64,10 +64,10 @@
 	<html xmlns="http://www.w3.org/1999/xhtml" lang="it" xml:lang="it">
 	<head>
 		<title>Games'Guide: Registrazione</title>
-		<meta name="description" content="Entra nella community di Games'Guide, troverai tutte le recensioni e le notizie sui giochi più recenti">
-		<meta name="author" content="Lucia Fenu, Francesco Battistella, Gianmarco Pettenuzzo, Matteo Depascale">
-		<meta name="keywords" content="Videogiochi, Notizie, Recensioni, Registrazione">
-		<meta name="robots" content="index, follow">
+		<meta name="description" content="Entra nella community di Games'Guide, troverai tutte le recensioni e le notizie sui giochi più recenti"/>
+		<meta name="author" content="Lucia Fenu, Francesco Battistella, Gianmarco Pettenuzzo, Matteo Depascale"/>
+		<meta name="keywords" content="Videogiochi, Notizie, Recensioni, Registrazione"/>
+		<meta name="robots" content="index, follow"/>
 		
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 		<link type="text/css" rel="stylesheet" href="CSS/Style.css" media="handheld, screen"/>
@@ -76,10 +76,11 @@
 		
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
 		
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		!--per jquery, host google-->
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
-		<script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
-		<script type="text/javascript" src="JS/goTop.js"></script>		
+		<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.js"></script>
+		<script type="text/javascript" src="JS/goTop.js"></script>
 	</head>
 	
 	<body>
