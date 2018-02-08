59cef381631058f7de0a92c86e2e0a80e03d12f5
Depaa <34038747+Depaa@users.noreply.github.com>
2018-02-08 15:47:39 +0100

    Update goTop.js

--- JS/goTop.js
+++ JS/goTop.js
@@ -1,14 +1,14 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.documentElement.scrollTop > 400)// || document.body.scrollTop > 400)
+    if (document.documentElement.scrollTop > 400 || document.body.scrollTop > 400)
         document.getElementById("backtotop").style.display = "block";
 	else 
         document.getElementById("backtotop").style.display = "none";
 }
 
-// When the user clicks on the button, scroll to the top of the document
+//premo bottone, torna su a 0px
 function gotopFunction() {
-    //document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 0; //Safari è speciale
+    document.documentElement.scrollTop = 0;
 }
