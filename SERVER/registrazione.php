132760b873f725c60a578c3b671cd954bec20f66
Depaa <34038747+Depaa@users.noreply.github.com>
2018-02-08 22:38:10 +0100

    ok

--- SERVER/registrazione.php
+++ SERVER/registrazione.php
@@ -76,7 +76,7 @@
 		
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
 		
-		!--per jquery, host google-->
+		<!--per jquery, host google-->
 		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 		<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.js"></script>
